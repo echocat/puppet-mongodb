12b423d4c80625e04f694f2643dbd41ed786fc0e
Daniel Werdermann <dwerdermann@web.de>
2015-01-19 13:53:45 +0100

    silent killall sigusr1

--- manifests/logrotate.pp
+++ manifests/logrotate.pp
@@ -21,8 +21,8 @@ class mongodb::logrotate {
     create_group  => $::mongodb::run_as_group,
     missingok     => true,
     ifempty       => false,
-    postrotate    => "  killall -SIGUSR1 mongod || true
-      killall -SIGUSR1 mongos || true
+    postrotate    => "  killall -SIGUSR1 mongod > /dev/null 2>&1 || true
+      killall -SIGUSR1 mongos > /dev/null 2>&1 || true
       find ${::mongodb::logdir} -type f -regex '.*\\.\\(log.[0-9].*-[0-9].*\\)' -exec rm {} \\;",
     require       => [Class['mongodb::install'], Class['mongodb::params']],
     before        => Anchor['mongodb::logrotate::end']
