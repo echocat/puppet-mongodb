3e46ca7f67c591e280aca11275df0f83416fcbdf
Daniel Werdermann <dwerdermann@web.de>
2015-11-03 13:23:10 +0100

    fix debian repo gpg key is too short #59

--- manifests/repos/apt.pp
+++ manifests/repos/apt.pp
@@ -9,7 +9,7 @@ class mongodb::repos::apt {
     location    => $source_location,
     release     => 'dist',
     repos       => '10gen',
-    key         => '7F0CEB10',
+    key         => '492EAFE8CD016A07919F1D2B9ECBEC467F0CEB10',
     key_server  => 'keyserver.ubuntu.com',
     include_src => false,
   }
