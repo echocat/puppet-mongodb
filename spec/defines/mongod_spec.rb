70b7c366aba099e95816d3bb59b2e2c6a471e9a3
Jochen Vetter <jochen.vetter@noris.de>
2017-09-12 13:31:59 +0200

    testing testing testing

--- spec/defines/mongod_spec.rb
+++ spec/defines/mongod_spec.rb
@@ -26,13 +26,6 @@ describe 'mongodb::mongod' , :type => :define do
       let(:params) {{ :mongod_deactivate_transparent_hugepage => true }}
       it { should contain_file("mongod_testdb_service").with_path("/etc/init.d/mongod_testdb").with_content(/\/sys\/kernel\/mm\/transparent_hugepage\//) }
     end
-    context 'with mongod_manage_service set to false' do
-      let(:params) {{ :mongod_manage_service => false }}
-      it {  should ! contain_service("mongod_testdb") }
-    end
-    context 'with mongod_manage_service unset' do
-      it {  should contain_service("mongod_testdb") }
-    end
   end
 
   context 'with defaults for all parameters on Debian' do
