f269361cd31d4269b2b551b56de3df041c4907e9
Jochen Vetter <jochen.vetter@noris.de>
2017-09-12 13:43:07 +0200

    Revert "testing testing testing"
    This reverts commit 46cd57325d81e8a2c04467160f7c7e10719694c0.
    
    dgdfgdg

--- manifests/repos/apt.pp
+++ manifests/repos/apt.pp
@@ -53,5 +53,9 @@ class mongodb::repos::apt (
       'id'     => '492EAFE8CD016A07919F1D2B9ECBEC467F0CEB10',
       'server' => 'keyserver.ubuntu.com',
     },
+    include  => {
+      'src' => false,
+      'deb' => true,
+    }
   }
 }
