1c085c0fb9eba3831aaaf94c5ed6381fe93ebf3e
Jochen Vetter <jochen.vetter@noris.de>
2017-09-12 14:27:01 +0200

    reverted changes req. puppet4

--- manifests/repos/apt.pp
+++ manifests/repos/apt.pp
@@ -46,16 +46,11 @@ class mongodb::repos::apt (
   }
 
   apt::source{ 'mongodb-source':
-    location => $location,
-    release  => $release,
-    repos    => $repos,
-    key      => {
-      'id'     => '492EAFE8CD016A07919F1D2B9ECBEC467F0CEB10',
-      'server' => 'keyserver.ubuntu.com',
-    },
-    include  => {
-      'src' => false,
-      'deb' => true,
-    }
+    location    => $location,
+    release     => $release,
+    repos       => $repos,
+    key         => '492EAFE8CD016A07919F1D2B9ECBEC467F0CEB10',
+    key_server  => 'keyserver.ubuntu.com',
+    include_src => false,
   }
 }
